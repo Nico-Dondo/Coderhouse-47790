9517352ac41fe4575146d76fb5e3f5c85c949a68
Nicolas Dondo <nicodondo1980@gmail.com>
2023-12-01 20:07:00 -0300

    Vistas y css

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,9 +1,11 @@
+# core/urls.py
 from django.urls import path
 from . import views
 
-app_name="core"
+app_name = "core"
 
 urlpatterns = [
-    path("", views.home, name ="index"),
+    path("", views.home, name="index"),
     path("about/", views.about, name="about"),
+    path("search/", views.search, name="search"),  # Agrega esta línea
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,7 +1,12 @@
+# core/views.py
 from django.shortcuts import render
 
 def home(request):
-    return render(request,"core/index.html" )
+    return render(request, "core/index.html")
 
 def about(request):
-    return render (request, "core/about.html")
+    return render(request, "core/about.html")
+
+def search(request):
+    # Lógica de búsqueda aquí
+    return render(request, "core/search_results.html")
